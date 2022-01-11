5cc2142c6e20be6093cf4a48be2586842f6b14a0
João Vítor G Douverny <douvernyjoaovitor@gmail.com>
2022-01-11 19:24:29 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
-import Navbar from "./componentes/Navbar";
 
+import  { 
+  BrowserRouter , 
+  Routes , 
+  Route 
+}  from  "react-router-dom" ; 
+import  Listing from  'pages/Listing'; 
+import  Form   from 'pages/Form'; 
+import  Navbar from "componentes/Navbar";
 
-function App() {
-  return (
-    <Navbar />
-  );
+function App () { 
+  return  ( 
+    < BrowserRouter > 
+      < Navbar  /> 
+      < Routes > 
+        < Route  path = "/" element={<Listing /> }/> 
+        < Route  path = "/form"> 
+          < Route  path= ":movieId " element={<Form />}/>
+        </ Route > 
+      </ Routes > 
+    </ BrowserRouter > 
+  ) ; 
 }
 
-export default App;
+export  default App;
---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,7 @@
+function Form(){
+
+    return(
+        <h1>Página de formulario</h1>
+    );
+}
+export default Form;
