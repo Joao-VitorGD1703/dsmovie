05bb37a80f6d1d78dfa280c56e67ef3eab9eaaf5
João Vítor G Douverny <douvernyjoaovitor@gmail.com>
2022-01-12 12:05:57 -0300

    Navegation Buttons

--- frontend/src/componentes/Navbar/MovieCard/indesx.tsx
+++ frontend/src/componentes/Navbar/MovieCard/indesx.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import MovieScore from "../MovieScore";
 
 function MovieCard() {
@@ -15,9 +16,12 @@ function MovieCard() {
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
                 <MovieScore />
-                <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+
+                <Link to="/form/1">
+                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                </Link>
             </div>
-        </div>
+        </div >
     );
 }
 export default MovieCard;
