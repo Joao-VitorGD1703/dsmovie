5962aa2a9e1da6351355a959243a5db032553d02
João Vítor G Douverny <douvernyjoaovitor@gmail.com>
2022-01-12 11:44:37 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,41 @@
+import MovieCard from "componentes/Navbar/MovieCard/indesx";
 import Pagination from "componentes/Navbar/Pagination";
 
-function Listing(){
+function Listing() {
 
-    return(
-        <Pagination/>
+    return (
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col=xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col=xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col=xl-3 mb-3 ">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col=xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col=xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col=xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                </div>
+            </div>
+        </>
     );
 }
 
