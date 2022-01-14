dfa5bdb137ffcb90f4de00f188a19914ae6c76b5
João Vítor G Douverny <douvernyjoaovitor@gmail.com>
2022-01-14 14:24:28 -0300

    Props

--- frontend/src/componentes/Navbar/MovieCard/indesx.tsx
+++ frontend/src/componentes/Navbar/MovieCard/indesx.tsx
@@ -1,14 +1,13 @@
 import { Link } from "react-router-dom";
+import { Movie } from "types/movie";
 import MovieScore from "../MovieScore";
 
-function MovieCard() {
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+type props = {
+    movie: Movie;
+}
+
+function MovieCard({movie} : props) {
+   
 
     return (
         <div>
@@ -17,7 +16,7 @@ function MovieCard() {
                 <h3>{movie.title}</h3>
                 <MovieScore />
 
-                <Link to="/form/1">
+                <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
             </div>
